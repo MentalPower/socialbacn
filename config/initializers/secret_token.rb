10a0efc07f006fcd509bd11b9ca01800fe0b4939
Esteban Santana Santana <MentalPower@MentalPower.us>
2013-02-01 20:34:01 -0600

    Initial app build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialbacn::Application.config.secret_token = '6680c5295efbd8729f53d911185c9d98ac2be94926f34404ef93f0ef77375d812df9cb9a910e440732c6e67746fa194cf4ea171b6d4a942a58d572dee2c4a81a'
