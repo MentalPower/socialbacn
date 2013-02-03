95d60856916dfb0e6e24554e07e9e1bfec3093c2
Esteban Santana Santana <MentalPower@MentalPower.us>
2013-02-03 17:03:48 -0600

    Make sure that we have the right twitter-related environment variables on startup, otherwise it all won't work :).

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,12 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+  #Make sure our required environment variables are present.
+  if ENV['TWITTER_KEY'].nil? || ENV['TWITTER_KEY'].empty?
+    raise ArgumentError.new("Required Twitter variable TWITTER_KEY missing")
+  elsif ENV['TWITTER_SECRET'].nil? || ENV['TWITTER_SECRET'].empty?
+    raise ArgumentError.new("Required Twitter variable TWITTER_SECRET missing")
+  end
+
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
