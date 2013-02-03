3f67643d8000eb160e7dab673f4b4d7bd933c7b9
Esteban Santana Santana <MentalPower@MentalPower.us>
2013-02-02 22:24:56 -0600

    Make sure we handle the case where there's no longer a record for that session.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
 private
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    begin
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
+      @current_user = nil
+    end
   end
   helper_method :current_user
 end
