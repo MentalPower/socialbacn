89f888d581e90a8b936006353ae8853cf0adbdba
Esteban Santana Santana <MentalPower@MentalPower.us>
2013-02-23 23:58:58 -0600

    Moved the secret token to being an environment variable instead of a hardcode.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Socialbacn::Application.config.secret_token = '6680c5295efbd8729f53d911185c9d98ac2be94926f34404ef93f0ef77375d812df9cb9a910e440732c6e67746fa194cf4ea171b6d4a942a58d572dee2c4a81a'
+
+if ENV['RAILS_SECRET_TOKEN'].nil? || ENV['RAILS_SECRET_TOKEN'].empty?
+  raise ArgumentError.new("Required Rails variable RAILS_SECRET_TOKEN missing")
+end
+
+Socialbacn::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']
