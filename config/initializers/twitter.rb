13f0d03f28b4968d41353280e094acb35cc97a76
Esteban Santana Santana <MentalPower@MentalPower.us>
2013-02-25 09:15:58 -0600

    Made sure that our env variables are there at this point too.

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,10 @@
 Twitter.configure do |config|
+  if ENV['TWITTER_KEY'].nil? || ENV['TWITTER_KEY'].empty?
+    raise ArgumentError.new("Required Twitter variable TWITTER_KEY missing")
+  elsif ENV['TWITTER_SECRET'].nil? || ENV['TWITTER_SECRET'].empty?
+    raise ArgumentError.new("Required Twitter variable TWITTER_SECRET missing")
+  end
+
   config.consumer_key = ENV['TWITTER_KEY']
   config.consumer_secret = ENV['TWITTER_SECRET']
 end
