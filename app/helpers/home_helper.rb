d49045b3bf75aea18d146759217263958e779e9e
Esteban Santana Santana <MentalPower@MentalPower.us>
2013-03-31 20:32:05 -0500

    We now reproduce both the user and home timelines using the helpers that we wrote earlier.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,10 @@
 module HomeHelper
+  def link_to_twitter(tweet)
+    link_to(
+      tweet.user.name + " (" + tweet.created_at.to_s + ")",
+      "https://twitter.com/" + tweet.user.name + "/status/" +
+      tweet.id.to_s,
+      :target => "_blank"
+    )
+  end
 end
